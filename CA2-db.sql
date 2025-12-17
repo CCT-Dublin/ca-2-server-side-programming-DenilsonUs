5acfa71c94fb8883479e9405d84e4d23c9c7779d
Denilson <urias4141@gmail.com>
2025-12-17 23:44:41 +0000

    Data Base creation / add xlsx file

---
+++ CA2-db.sql
@@ -0,0 +1,12 @@
+ CREATE DATABASE IF NOT EXISTS Server_side_Ca2 ;
+USE Server_side_Ca2 ;
+CREATE TABLE mysql_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(20),
+    second_name VARCHAR(20),
+    email VARCHAR(100),
+    phone_number VARCHAR(10),
+    eircode VARCHAR(6)
+);
+
+select * from mysql_table; 
